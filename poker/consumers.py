f3295a45fa7de149b06868da69238d8c4ee7a5c3
Drew Springfield <drew@robosim.com>
2024-07-16 22:41:56 -0700

    14th

--- poker/consumers.py
+++ poker/consumers.py
@@ -1,13 +1,16 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .game import PokerGame
 
 class PokerConsumer(AsyncWebsocketConsumer):
+    #Game instances
+    games = {}
+    #player id to consumer instance maps
+    player_to_consumer = {}
+    
     async def connect(self):
         print("Attempting to connect")
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'poker_{self.room_name}'
-        self.game = PokerGame()
 
         # Join room group
         await self.channel_layer.group_add(
@@ -41,21 +44,50 @@ class PokerConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
-    async def poker_action(self, event):
-        action = event['action']
-        user_id = event['user_id']
+    async def start_new_hand(self):
+        game_state = self.game.start_new_hand()
+        await self.send_private_hands(game_state)
+        await self.send_public_game_state(game_state)
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'action': action,
-            'user_id': user_id
-        }))
+    async def process_action(self, action):
+        if self.game.street == 'preflop':
+            valid_actions = self.game.get_valid_preflop_actions()
+            if action in valid_actions:
+                self.game.process_preflop_action(action)
+        else:
+            valid_actions = self.game.get_valid_postflop_actions()
+            if action in valid_actions:
+                self.game.process_postflop_action(action)
 
-    async def handle_start_hand(self, data):
-        state = self.game.start_new_hand()
+        game_state = self.game.get_game_state()
 
-        await self.send(text_data=json.dumps({
-            'type': 'game_state',
-            'game_state': game_state,
-        }))
+        if game_state['hand_over']:
+            result = self.game.determine_showdown_winner()
+            game_state.update(result)
+
+    async def send_private_hands(self, game_state):
+        for player in [game_state['oop_player'], game_state['ip_player']]:
+            if player['id'] in self.player_to_consumer:
+                consumer = self.player_to_consumer[player['id']]
+                await consumer.send(text_data=json.dumps({
+                    'type': 'private_hand',
+                    'hand': player['hand']    
+                }))
+
+    async def send_public_game_state(self, game_state):
+        #Remove hands from game_state
+        public_state = game_state.copy()
+        public_state['oop_player'] = {k: v for k, v in public_state['oop_player'].items() if k != 'hand'}
+        public_state['ip_player'] = {k: v for k, v in public_state['ip_player'].items() if k != 'hand'}
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'game_state_update',
+                'game_state': public_state
+            }
+        )
+
+    async def game_state_update(self, event):
+        game_state = event['game_state']
+        await self.send(text_data=json.dumps(game_state))
