e06470c8744495385021ef8e20886b664402ed98
wrigglyt <ryan@monocomputing.com>
2024-08-24 12:22:44 -0700

    update folder

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 cd /opt/frontend
 nohup /bin/bash -c npm run start &
+nohup python3 /opt/ai/train.py &
 cd /opt/
 daphne -b 0.0.0.0 -p 8000 plo_project.asgi:application
 #nohup /bin/sh -c python3 /opt/poker/cli_game &
