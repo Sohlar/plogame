9b57d1a25a33f1358f8f009eb2ba900812af0c60
wrigglyt <ryan@monocomputing.com>
2024-08-24 19:32:21 -0700

    clear venv

--- decision_tree/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import PotType, Position, Player, FlopTexture, BetSize, BetFrequency
-
-admin.site.register(PotType)
-admin.site.register(Position)
-admin.site.register(Player)
-admin.site.register(FlopTexture)
-admin.site.register(BetSize)
-admin.site.register(BetFrequency)
--- frontend/src/components/Game.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-
-function Game() {
-  return <h2>Poker Game</h2>;
-}
-
-export default Game;
