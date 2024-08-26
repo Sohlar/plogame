d744d633b0f4ad14dfe1328e84c346bdbad60c0b
wrigglyt <ryan@monocomputing.com>
2024-08-26 14:16:40 -0700

    db in compose and db init done

--- game/Frontend_Middleware/db_connect.py
+++ game/Frontend_Middleware/db_connect.py
@@ -1,10 +1,10 @@
 import psycopg2
 
-connection = psycopg2.connect(database="postgres", user="postgres", password="changeme", host="localhost", port=5432)
+connection = psycopg2.connect(database="plo", user="postgres", password="changeme", host="localhost", port=5432)
 
 cursor = connection.cursor()
 
-cursor.execute("SELECT 1;")
+cursor.execute("select * from game where status = 'ongoing'")
 
 # Fetch all rows from database
 record = cursor.fetchall()
