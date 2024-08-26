afce995280d3f8b4ce60fa60c11933610f32a558
wrigglyt <ryan@monocomputing.com>
2024-08-26 14:26:29 -0700

    db section and initial schema completed

--- game/Frontend_Middleware/websocket_client.py
+++ game/Frontend_Middleware/websocket_client.py
@@ -3,7 +3,6 @@
 import asyncio
 from websockets.sync.client import connect
 
-<<<<<<< HEAD
 uri = "localhost"
 port = "8765"
 
@@ -17,12 +16,3 @@ test("{\"id\": \"1\", \"name\": \"demo\", \"count\": \"0\", \"action\": \"call:0
 test("{\"id\": \"1\", \"name\": \"demo\", \"count\": \"0\", \"action\": \"bet:50\"}")
 test("{\"id\": \"1\", \"name\": \"demo\", \"count\": \"0\", \"action\": \"fold:0\"}")
 test("{\"id\": \"1\", \"name\": \"demo\", \"count\": \"0\", \"action\": \"check:0\"}")
-=======
-def hello():
-    with connect("ws://localhost:8765") as websocket:
-        websocket.send("call")
-        message = websocket.recv()
-        print(f"Received: {message}")
-
-hello()
->>>>>>> master
